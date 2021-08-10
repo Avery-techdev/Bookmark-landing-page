cfeff0e4d8e0d98519bcdba8a2195cfb8345721d
Avery-techdev <77972529+Avery-techdev@users.noreply.github.com>
2021-08-10 13:00:17 +0200

    extracted the script in an extra file

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 function openNav() {
   document.getElementById("open").style.display = "none";
-  document.getElementById("logo").style.visibility= "hidden";
+  document.getElementById("logo").style.visibility = "hidden";
   document.getElementById("nav").style.width = "100%";
-
-};
+}
 function closeNav() {
   document.getElementById("nav").style.width = "0";
   document.getElementById("open").style.display = "block";
   document.getElementById("logo").style.visibility = "visible";
-};
+}
 
 function switchContentSimple() {
   document.getElementById("btn-simple").style.color = "black";
   document.getElementById("under-Bookmarking").style.display = "block";
-};
+}
 
 function switchContentSpeedy() {
   document.getElementById("btn-speedy").style.color = "black";
   document.getElementById("under-Bookmarking").style.display = "block";
-};
+}
 
 function switchContentEasy() {
   document.getElementById("btn-easy").style.color = "black";
   document.getElementById("under-Bookmarking").style.display = "block";
-};
+}
